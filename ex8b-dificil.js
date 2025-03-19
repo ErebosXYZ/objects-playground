4d62378d738fc1308c0f9f70053bb8ff4a730b2a
ErebosXYZ <robertmaynou@gmail.com>
2025-03-19 22:02:38 +0100

    mirar comentaris

--- ex8b-dificil.js
+++ ex8b-dificil.js
@@ -4,10 +4,24 @@
  * Recibe un array de personas (objetos con propiedad "pais") y un string con el nombre de un país.
  * Devuelve cuántas personas en el array son de ese país.
  */
+// Necessitem saber si people.pais es repeteix i si ho fa, retornar quantes vegades ho fa. Si people.pais (la propietat pais de l'objecte)
 
 function countPeopleFromCountry(people, country) {
-
+    let countryPeople = 0;
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].pais === country) {
+            countryPeople++;
+        }
+    } 
+    return countryPeople;
 }
+
+// creo la variable countryPeople que fa de comptador i s'inicialitza a 0. 
+// Amb el for, recorro l'array people 3 vegades, incrementant l'índex de consulta. 
+// Si en aquest recorregut l'índex on estem de people té un país que és igual a country, el comptador s'incrementa: countryPeople++
+// fem que la funció retorni el nombre total del comptador. No posem return ni a l'if ni al for.
+// Si posem return a l'if, ens retorna sempre 0 perquè el return actua abans d'incrementar el nombre. 
+// Si posem return al for només recorrerà el primer objecte i per tant retornarà només el valor 1 i després sortirà del bucle
 
 // Ejemplo de uso:
 console.log(countPeopleFromCountry([
